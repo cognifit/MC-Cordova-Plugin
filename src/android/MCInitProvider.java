aca935aa613fe809c1b8d5067dbae55bb8d72cf3
Pedro Gutierrez <pedro.gutierrez@madideas.es>
2020-11-30 12:10:22 +0100

    added missing import

--- src/android/MCInitProvider.java
+++ src/android/MCInitProvider.java
@@ -33,6 +33,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.salesforce.marketingcloud.InitializationStatus;
